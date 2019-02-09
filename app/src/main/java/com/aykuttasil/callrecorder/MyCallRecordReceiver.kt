2a7882500f922bfa74956ca883094411382fc7a4
Aykut Asil <aykuttasil@gmail.com>
2019-02-09 15:15:18 +0300

    update

--- app/src/main/java/com/aykuttasil/callrecorder/MyCallRecordReceiver.kt
+++ app/src/main/java/com/aykuttasil/callrecorder/MyCallRecordReceiver.kt
@@ -9,6 +9,7 @@ import java.util.Date
 
 class MyCallRecordReceiver(callRecord: CallRecord) : CallRecordReceiver(callRecord) {
 
+
     override fun onIncomingCallReceived(context: Context, number: String?, start: Date) {
         super.onIncomingCallReceived(context, number, start)
     }
